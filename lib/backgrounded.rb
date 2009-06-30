b5a24606092154ecaaadc3458824bd3a1680b1b0
Ryan Sonnek <ryan@socialcast.com>
2009-06-30 16:00:17 -0500

    extract handler for invoking in background

--- lib/backgrounded.rb
+++ lib/backgrounded.rb
@@ -1,25 +1,43 @@
-module Backgrounded
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
+require 'activesupport'
 
-  module ClassMethods
-    def backgrounded(*methods)
-      methods.each do |method|
-        define_method "#{method.to_s}_in_background" do |*args|
-          self.send method, *args
-        end
+module Backgrounded
+  mattr_accessor :handler
+  def self.handler
+    @@handler ||= Backgrounded::Handler::InprocessHandler.new
+  end
+  module Handler
+    #simple handler to process synchronously and not actually in the background
+    #useful for testing
+    class InprocessHandler
+      def request(object, method, *args)
+        object.send method, *args
       end
-      include Backgrounded::InstanceMethods
-      extend Backgrounded::SingletonMethods
     end
   end
 
-  module SingletonMethods
-  end
+  module Model
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def backgrounded(*methods)
+        methods.each do |method|
+          define_method "#{method.to_s}_in_background" do |*args|
+            Backgrounded.handler.request(self, method, *args)
+          end
+        end
+        include Backgrounded::Model::InstanceMethods
+        extend Backgrounded::Model::SingletonMethods
+      end
+    end
+
+    module SingletonMethods
+    end
   
-  module InstanceMethods
+    module InstanceMethods
+    end
   end
 end
 
-Object.send(:include, Backgrounded)
+Object.send(:include, Backgrounded::Model)
