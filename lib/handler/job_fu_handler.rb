e04424ffd91c5da311042aceff8337d14a6ac356
Jon Stenqvist <jon.stenqvist@gmail.com>
2009-08-07 16:00:28 +0200

    Namespacing Job-model

--- lib/handler/job_fu_handler.rb
+++ lib/handler/job_fu_handler.rb
@@ -8,7 +8,7 @@ module Backgrounded
       def request(object, method, *args)
         opt = args.extract_options!
         priority, process_at = opt[:priority], opt[:at]
-        Job.enqueue ProcessableMethod.new(object, method, *args), priority, process_at
+        JobFu::Job.enqueue ProcessableMethod.new(object, method, *args), priority, process_at
       end
     end
   end
