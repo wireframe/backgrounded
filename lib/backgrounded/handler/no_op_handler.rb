102ec590d8f530cb3380b5511abdbd6ec78af1f9
Ryan Sonnek <ryan@socialcast.com>
2010-12-02 07:44:25 -0800

    add no-op handler

---
+++ lib/backgrounded/handler/no_op_handler.rb
@@ -0,0 +1,11 @@
+module Backgrounded
+  module Handler
+    # throw requests out the window
+    # useful for test environment to avoid any background work
+    class NoOpHandler
+      def request(object, method, *args)
+        # do nothing
+      end
+    end
+  end
+end
