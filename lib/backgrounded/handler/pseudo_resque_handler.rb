493b398c93ed5c83ff6b509684c66281411ed540
Ryan Sonnek <ryan@socialcast.com>
2011-06-02 11:46:34 -0500

    add pseudo resque handler

---
+++ lib/backgrounded/handler/pseudo_resque_handler.rb
@@ -0,0 +1,14 @@
+require 'resque'
+
+# handler that acts like the in process handler but marshalls the arguments
+# this simulates how resque encodes/decodes values to/from redis
+# useful when passing symbols to arguments and they end up being processed as strings
+module Backgrounded
+  module Handler
+    class PseudoResqueHandler
+      def request(object, method, *args)
+        object.send method, *Resque.decode(Resque.encode(args))
+      end
+    end
+  end
+end
